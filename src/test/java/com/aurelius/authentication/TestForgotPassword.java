842fe3866e03564855f662f1ddf53f53bcdac020
Satish Gaware <167776678+satishgaware123@users.noreply.github.com>
2024-12-05 15:45:17 +0530

    Revert "A 02 authentication (#8)"
    This reverts commit d1ebc108ffb1a96ea20e8f76801f8b1ed90c60d8.

--- src/test/java/com/aurelius/authentication/TestForgotPassword.java
+++ src/test/java/com/aurelius/authentication/TestForgotPassword.java
@@ -2,8 +2,13 @@ package com.aurelius.authentication;
 
 
 import org.testng.annotations.Test;
+
+import org.testng.annotations.Test;
+
 import com.aurelius.base.BaseClass;
+
 import org.testng.Assert;
+
 
 public class TestForgotPassword extends BaseClass {
 
