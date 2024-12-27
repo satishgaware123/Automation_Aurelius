8bdacc96f24982a9060efe1e1fca245b69b37e9f
satish gaware <satishgaware999@gmail.com>
2024-12-27 12:37:31 +0530

    new changes

--- src/test/java/com/aurelius/authentication/TestForgotPassword.java
+++ src/test/java/com/aurelius/authentication/TestForgotPassword.java
@@ -7,14 +7,14 @@ import com.aurelius.base.BaseClass;
 
 public class TestForgotPassword extends BaseClass {
 
-	@Test
+	@Test(priority = 1)
 	public void verifyThatNavigateToTheForgotPassword() throws InterruptedException {
 		driver.get("http://3.25.145.165/auth");
 		forgotPasswordPom.clickOnTheForgotPass();
 		Assert.assertEquals(driver.getCurrentUrl(), "http://3.25.145.165/auth/forgot-password");
 	}
 
-	@Test
+	@Test(priority = 2)
 	public void verifyThatEnterTheInvalidEmailInForgotPassword() throws InterruptedException {
 		driver.get("http://3.25.145.165/auth");
 		forgotPasswordPom.clickOnTheForgotPass();
